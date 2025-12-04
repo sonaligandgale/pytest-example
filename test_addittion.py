13ac165677565f8c75832d7400918c800431d7a1
sonaligandgale <sonaligandgalekar@gmail.com>
2025-12-04 14:32:15 +0530

    Update and rename testaddittion.py to test_addittion.py

---
+++ test_addittion.py
@@ -0,0 +1,10 @@
+from addition import add   # fixed spelling: 'addition' not 'addittion'
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_add_negative_numbers():
+    assert add(-4, -5) == -9  
+
+def test_add_zero():
+    assert add(0, 5) == 5
